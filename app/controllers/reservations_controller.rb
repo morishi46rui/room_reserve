75222c6c8d547bc8b321470e6a00e4364502a345
宮森瑠偉 <miyamorirui@miyamoriruinoMacBook-Air.local>
2023-01-03 23:31:45 +0900

    カードの登録がされていない場合予約時に登録画面に遷移するよう設定

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,19 +6,24 @@ class ReservationsController < ApplicationController
 
     if current_user == room.user
       flash[:alert] = "オーナーが予約することはできません。"
+    elsif current_user.stripe_id.blank?
+      flash[:alert] = "予約する前にクレジットカードを登録する必要があります。"
+      return redirect_to settings_payment_path
     else
 
       start_date = Date.parse(reservation_params[:start_date])
       end_date = Date.parse(reservation_params[:end_date])
       days = (end_date - start_date).to_i + 1
-
+      if days < 2
+        flash[:alert] = "宿泊日数が１泊以上でなければ予約することはできません。"
+      else
       @reservation = current_user.reservations.build(reservation_params)
       @reservation.room = room
       @reservation.price = room.price
       @reservation.total = room.price * days
       @reservation.save
-
       flash[:notice] = "予約が完了しました。"
+      end
     end
       redirect_to room
   end
@@ -32,7 +37,36 @@ class ReservationsController < ApplicationController
   end
 
   private
-    def reservation_params
-      params.require(:reservation).permit(:start_date, :end_date)
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
+
+  def charge(room, reservation)
+    host_amount = (reservation.total * 0.8).to_i 
+    if !reservation.user.stripe_id.blank?
+      customer = Stripe::Customer.retrieve(reservation.user.stripe_id)
+      charge = Stripe::Charge.create(
+        :customer => customer.id,
+        :amount => reservation.total,
+        :description => room.listing_name,
+        :currency => "jpy",
+        transfer_data: {
+          amount: host_amount, 
+          destination: room.user.merchant_id, 
+        },
+      )
+
+      if charge
+        reservation.Approved!
+        flash[:notice] = "お支払い手続きが完了し、ご予約されました。お越しをお待ちしております！"
+      else
+        reservation.Declined!
+        flash[:notice] = "お支払い手続きができません。予約ができませんでした。"
+      end
     end
+  rescue Stripe::CardError => e
+    reservation.declined!
+    flash[:alert] = e.message
+  end
+
 end
